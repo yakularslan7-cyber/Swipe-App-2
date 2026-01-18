f45bc8a99d9e82e7287057155c60ca67f5195a62
yakularslan7-cyber <yakularslan7@gmail.com>
2026-01-18 22:17:44 +0300

    Update service-worker.js

--- swipe/service-worker.js
+++ swipe/service-worker.js
@@ -1,20 +1,12 @@
-const CACHE_NAME = "single-html-pwa-v1";
+const CACHE_NAME = "pwa-ghpages-v1";
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
-        "/",
-        "/index.html"
+        "./",
+        "./index.html"
       ]);
-    })
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
     })
   );
 });
